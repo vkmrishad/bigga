cc2277a93bfdba9f92bd01a958d0a4dd0a40b319
dhilipsiva <dhilipsiva@pm.me>
2020-03-20 14:34:40 +0530

    Create machind

--- scripts/docker-machie-create.sh
+++ scripts/docker-machie-create.sh
@@ -9,7 +9,17 @@ doctl compute region list
 # Create Machine
 docker-machine create \
     --driver digitalocean \
-    --digitalocean-access-token <TOKEN> \
-    --digitalocean-size 512mb \
+    --digitalocean-access-token $DO_TOKEN \
+    --digitalocean-size 4gb \
     --digitalocean-region blr1 \
+    myinstance
+
+
+docker-machine create \
+    --driver amazonec2 \
+    --amazonec2-access-key $AWS_ACCESS_KEY_ID \
+    --amazonec2-secret-key $AWS_SECRET_ACCESS_KEY  \
+    --amazonec2-vpc-id $AWS_VPC_ID \
+    --amazonec2-region $AWS_DEFAULT_REGION \
+    --amazonec2-instance-type a1.xlarge \
     myinstance
